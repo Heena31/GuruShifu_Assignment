fcd4e74bb4af3685f0877471e9dd645c6992dab3
Heena Garg <heena1502.cse19@chitkara.edu.in>
2023-10-25 17:14:15 +0000

    Add. Area Method Implementation

---
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.rectangle;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+
+public class RectangleTest {
+    
+@Test
+
+public void shouldReturnAreaWhenTheLengthAndBreadthAreGiven()
+{
+    Rectangle rt=new Rectangle(3.14,2.675);
+    double area= rt.area();
+    assertThat(area, is(closeTo(8.3995, 0.01)));
+
+}
+}
+
